75cb7ca525558d3ad37458f7d9ca74cf519a1d1f
xramadhann <xxdhanz@gmail.com>
2024-08-14 14:32:26 +0700

    feat : create file thirdScreen

---
+++ testintern/lib/views/thirdScreen.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: file_names, camel_case_types
+import 'package:flutter/material.dart';
+
+class thirdScreen extends StatefulWidget {
+  const thirdScreen({super.key});
+
+  @override
+  State<thirdScreen> createState() => _thirdScreenState();
+}
+
+class _thirdScreenState extends State<thirdScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
