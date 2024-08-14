fbfa191afde792f35f00c58988a9ac7ca64084dc
xramadhann <xxdhanz@gmail.com>
2024-08-14 15:12:28 +0700

    feat : create secondScreen.dart

---
+++ testintern/lib/views/secondScreen.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: file_names, camel_case_types
+import 'package:flutter/material.dart';
+
+class secondScreen extends StatefulWidget {
+  const secondScreen({super.key});
+
+  @override
+  State<secondScreen> createState() => _secondScreenState();
+}
+
+class _secondScreenState extends State<secondScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
