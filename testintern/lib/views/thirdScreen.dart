7885f63f9106d0d0a8c9112e08e4ce79f01852fd
xramadhann <xxdhanz@gmail.com>
2024-08-14 14:59:23 +0700

    style : styleing app bar and list view

--- testintern/lib/views/thirdScreen.dart
+++ testintern/lib/views/thirdScreen.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: file_names, camel_case_types, unnecessary_string_interpolations
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:testintern/Models/dataUser.dart';
+import 'package:testintern/views/firstScreen.dart';
 
 class thirdPage extends StatefulWidget {
   const thirdPage({super.key});
@@ -15,7 +17,20 @@ class _thirdPageState extends State<thirdPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Third Screen'),
+        title: const Center(
+          child: Text(
+            "Third Screen",
+          ),
+        ),
+        leading: IconButton(
+          icon: const Icon(CupertinoIcons.chevron_back),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const firstScreen()),
+            );
+          },
+        ),
       ),
       body: SingleChildScrollView(
         child: Center(
