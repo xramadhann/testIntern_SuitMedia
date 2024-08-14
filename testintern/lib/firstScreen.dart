767f7980395753235feb6fad9d7b10dc68e78aa9
xramadhann <xxdhanz@gmail.com>
2024-08-14 13:49:08 +0700

    feat : button validation and static image asset

--- testintern/lib/firstScreen.dart
+++ testintern/lib/firstScreen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names, camel_case_types
+// ignore_for_file: file_names, camel_case_types, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 
@@ -31,6 +31,17 @@ class _firstScreenState extends State<firstScreen> {
               padding: const EdgeInsets.all(20),
               child: Column(
                 children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.90,
+                    height: 150,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: NetworkImage("https://picsum.photos/200"),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 50),
                   Container(
                     width: MediaQuery.of(context).size.width * 0.90,
                     height: 50,
@@ -73,6 +84,52 @@ class _firstScreenState extends State<firstScreen> {
                     ),
                   ),
                 ],
+              ),
+            ),
+            const SizedBox(height: 40),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  checkPalindrome();
+                });
+              },
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.90,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Center(
+                  child: Text(
+                    'CHECK',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.90,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Center(
+                  child: Text(
+                    'NEXT',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
               ),
             ),
           ],
