31851e2ce6e5dd0def96a607041bae7c59f3751f
xramadhann <xxdhanz@gmail.com>
2024-08-14 13:32:29 +0700

    feat : create method of polindrom

--- testintern/lib/firstScreen.dart
+++ testintern/lib/firstScreen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: file_names, camel_case_types
+
 import 'package:flutter/material.dart';
 
 class firstScreen extends StatefulWidget {
@@ -7,10 +9,20 @@ class firstScreen extends StatefulWidget {
   State<firstScreen> createState() => _firstScreenState();
 }
 
+String inputText = '';
+bool isPalindrome = false;
+
+void checkPalindrome() {
+  String cleanedText =
+      inputText.replaceAll(RegExp(r'[^a-zA-Z0-9]'), '').toLowerCase();
+  String reversedText = cleanedText.split('').reversed.join('');
+  isPalindrome = cleanedText == reversedText;
+}
+
 class _firstScreenState extends State<firstScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
         child: Text("Hello World"),
       ),
